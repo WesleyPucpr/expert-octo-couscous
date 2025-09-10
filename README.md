0ca637da49ecfbc1e9777c894f7ee201f4a572fa
Wesley Costa <c.wesley@pucpr.edu.br>
2025-09-10 18:19:00 -0300

    Configuração Inicial
    Configuração inicial de uma API utilizando o FastAPI.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # expert-octo-couscous
+Olá, Este é um repositorio *exclusivo para testes*.Pesquise mais sobre Markdown para aprender algumas coisas sobre formatação de textos para arquivos Readme como este.
---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+App = FastAPI()
+
+# 127.0.0.1:8000/
+@App.get("/")
+async def root():
+    return {"massage": "Hello World"}
+
+# 127.0.0.1:8000/teste1
+@App.get("/teste1")
+async def funcaoteste():
+    return {"teste": "deu certoo"}
